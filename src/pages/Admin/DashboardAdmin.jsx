1e557a21c76a131e474daaf878f9e084c1e4152b
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2025-05-04 15:41:20 +0700

    Fix name file

--- src/pages/Admin/DashboardAdmin.jsx
+++ src/pages/Admin/DashboardAdmin.jsx
@@ -1,7 +1,7 @@
 import * as LucideIcons from "lucide-react";
-import StatCard from "@/components/Dashboard/stat-card";
-import RecentCourses from "@/components/Dashboard/recent-course";
-import RevenueChart from "@/components/Dashboard/revenue-chart";
+import StatCard from "@/components/Dashboard/StatCard";
+import RecentCourses from "@/components/Dashboard/RecentCourses";
+import RevenueChart from "@/components/Dashboard/RevenueChart";
 import { useEffect, useState } from "react";
 import {
   fetchDashboardStats,
